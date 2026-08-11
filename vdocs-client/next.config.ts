0614fad1b26a3c012f84e947d06a71746adf766b
Nguyễn Ninh <nguyenvanninh2004pt@gmail.com>
2026-08-11 09:34:27 +0700

    init commit 29

--- vdocs-client/next.config.ts
+++ vdocs-client/next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   allowedDevOrigins: ["192.168.204.51"],
 };
 

610fb5975cd7f1dc9bc38ea6d0aedc04aa1af195
Nguyễn Ninh <nguyenvanninh2004pt@gmail.com>
2026-08-13 17:04:51 +0700

    Use markdown for about pages

--- vdocs-docs/mdx-components.tsx
+++ vdocs-docs/mdx-components.tsx
@@ -1,5 +1,4 @@
 import { useMDXComponents as getDocsMDXComponents } from "nextra-theme-docs";
-import { GameLanding } from "./components/GameLanding";
 import { Markmap } from "./components/Markmap";
 
 const docsComponents = getDocsMDXComponents();
@@ -7,7 +6,6 @@ const docsComponents = getDocsMDXComponents();
 export function useMDXComponents(components: Record<string, unknown> = {}) {
   return {
     ...docsComponents,
-    GameLanding,
     Markmap,
     ...components,
   };
